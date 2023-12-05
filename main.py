5f45c463ece556556cea0554b884aaa06006dd31
WutIsHummus <65522410+WutIsHummus@users.noreply.github.com>
2023-12-05 12:12:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class MyBot(commands.Bot):
         if self.user.mentioned_in(message) and not message.mention_everyone:
             self.ping_count += 1  # Increment the counter
             query_elements = ["silly", "cute", "funny", "adorable", "playful", "furry", "small", "fluffy", "colorful", "small", "tiny", "shy", "soft", "fat", "cuddly", "curious"] #Add adjectives here to vary query results
-            selected_elements = random.sample(query_elements, 3)
+            selected_elements = random.sample(query_elements, 2)
             query = "Cats ".join(selected_elements)  # Combine the elements into a query string
             start_index = self.ping_count  # Use the counter as the start index for image search
             images = search_images(query, GOOGLE, SEARCH, start=start_index)  
